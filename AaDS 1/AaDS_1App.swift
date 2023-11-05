dc7f6f3926e138b8d38df3450d67dca96ce57551
F0de <paper_letter@icloud.com>
2023-11-05 18:38:19 +0200

    Initial Commit

---
+++ AaDS 1/AaDS_1App.swift
@@ -0,0 +1,17 @@
+//
+//  AaDS_1App.swift
+//  AaDS 1
+//
+//  Created by Влад Тимчук on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AaDS_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
